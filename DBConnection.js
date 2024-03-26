92b8ad31fc77c33849d477adcb7ef5f44992dbb9
soumyasics <soumya.sics@gmail.com>
2024-03-26 23:02:30 +0530

    first

--- DBConnection.js
+++ DBConnection.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose")
-mongoose.connect("mongodb://127.0.0.1:27017/packers")
+mongoose.connect("mongodb://127.0.0.1:27017/freelancers")
 var db=mongoose.connection
 db.on("error",console.error.bind("error"))
 db.once("open",function(){
