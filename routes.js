c7624e2b71bf03f902d3486d80ac8308ef4d84ca
soumyasics <soumya.sics@gmail.com>
2024-04-04 14:01:28 +0530

    fe

--- routes.js
+++ routes.js
@@ -5,5 +5,6 @@ const freelancer=require('./Freelancers/freelancerController')
 router.post('/freelancerRegistration',freelancer.upload,freelancer.freelancerRegistration)
 router.post('/getAllFreelancers',freelancer.getAllFreelancers)
 router.post('/getFreelancerById/:id',freelancer.getFreelancerById)
+router.post('/loginFreelancer',freelancer.loginFreelancer)
 
 module.exports=router
