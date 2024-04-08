30888da7b3642deb5de62bfc9863713ce62cb8ac
Anand R P <anandrp.sics@gmail.com>
2024-04-08 19:00:16 +0530

    middlewared added on main file

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const route=require('./routes')
 const cors=require('cors')
 
 const app=express()
-
+app.use(express.static(`${__dirname}/upload`));
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.use(cors())
 
